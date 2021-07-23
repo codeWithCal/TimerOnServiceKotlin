9d88540f56899bc4e1dd7da086335ec257db793c
Callum Hill <ccahill.is@gmail.com>
2021-07-23 13:48:36 +1000

    Initial Commit

---
+++ app/src/main/java/code/with/cal/timeronservicetutorial/MainActivity.kt
@@ -0,0 +1,85 @@
+package code.with.cal.timeronservicetutorial
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.format.Time
+import code.with.cal.timeronservicetutorial.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+    private var timerStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startStopButton.setOnClickListener { startStopTimer() }
+        binding.resetButton.setOnClickListener { resetTimer() }
+
+        serviceIntent = Intent(applicationContext, TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
+    }
+
+    private fun resetTimer()
+    {
+        stopTimer()
+        time = 0.0
+        binding.timeTV.text = getTimeStringFromDouble(time)
+    }
+
+    private fun startStopTimer()
+    {
+        if(timerStarted)
+            stopTimer()
+        else
+            startTimer()
+    }
+
+    private fun startTimer()
+    {
+        serviceIntent.putExtra(TimerService.TIME_EXTRA, time)
+        startService(serviceIntent)
+        binding.startStopButton.text = "Stop"
+        binding.startStopButton.icon = getDrawable(R.drawable.ic_baseline_pause_24)
+        timerStarted = true
+    }
+
+    private fun stopTimer()
+    {
+        stopService(serviceIntent)
+        binding.startStopButton.text = "Start"
+        binding.startStopButton.icon = getDrawable(R.drawable.ic_baseline_play_arrow_24)
+        timerStarted = false
+    }
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver()
+    {
+        override fun onReceive(context: Context, intent: Intent)
+        {
+            time = intent.getDoubleExtra(TimerService.TIME_EXTRA, 0.0)
+            binding.timeTV.text = getTimeStringFromDouble(time)
+        }
+    }
+
+    private fun getTimeStringFromDouble(time: Double): String
+    {
+        val resultInt = time.roundToInt()
+        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400 % 3600 / 60
+        val seconds = resultInt % 86400 % 3600 % 60
+
+        return makeTimeString(hours, minutes, seconds)
+    }
+
+    private fun makeTimeString(hour: Int, min: Int, sec: Int): String = String.format("%02d:%02d:%02d", hour, min, sec)
+}
---
+++ app/src/main/java/code/with/cal/timeronservicetutorial/TimerService.kt
@@ -0,0 +1,44 @@
+package code.with.cal.timeronservicetutorial
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import java.util.*
+
+class TimerService : Service()
+{
+    override fun onBind(p0: Intent?): IBinder? = null
+
+    private val timer = Timer()
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int
+    {
+        val time = intent.getDoubleExtra(TIME_EXTRA, 0.0)
+        timer.scheduleAtFixedRate(TimeTask(time), 0, 1000)
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy()
+    {
+        timer.cancel()
+        super.onDestroy()
+    }
+
+    private inner class TimeTask(private var time: Double) : TimerTask()
+    {
+        override fun run()
+        {
+            val intent = Intent(TIMER_UPDATED)
+            time++
+            intent.putExtra(TIME_EXTRA, time)
+            sendBroadcast(intent)
+        }
+    }
+
+    companion object
+    {
+        const val TIMER_UPDATED = "timerUpdated"
+        const val TIME_EXTRA = "timeExtra"
+    }
+
+}
